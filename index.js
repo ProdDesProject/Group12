4ee366d7302b4d6ae013d3f6306722502d415ddf
nipe1 <pelkonenniilo@gmail.com>
2020-11-28 20:45:47 +0200

    added some xp gain func

--- index.js
+++ index.js
@@ -16,28 +16,73 @@ app.get('/skills', (req, res) => {
   const t = skills.getAllSkills();
         res.json(t);
 })
-app.get('/skills/id=:id', (req, res) => {
-  const t = skills.getSkillsById(req.params.id)
-      if(t !== undefined){
-        res.json(t)
-      }
-      else{
-        res.sendStatus(404)
-      }
-})
 
 app.get('/skills/username=:username', (req, res) => {
-  const t = skills.getSkillsByName(req.params.username)
-      if(t !== undefined){
-        res.json(t)
-      }
-      else{
-        res.sendStatus(404)
-      }
+  const u = skills.getSkillsByName(req.params.username);
+  if(u !== undefined){
+    res.json(u);
+  }
+  else{
+    res.sendStatus(404);
+  }
 })
 
-app.post('/skills', (req, res) => {
+app.post('/startgains', (req, res) => {
 
+  if('username' in req.body == false ) {
+    return res.status(400).json({
+        status: 'error',
+        error: 'Missing username value from body'
+    });
+  }
+  skills.createGains(req.body.username);
+}),
+
+app.post('/gains', (req, res) => {
+
+  if('username' in req.body == false ) {
+    return res.status(400).json({
+        status: 'error',
+        error: 'Missing username value from body'
+    });
+  }
+  if('gained_xp' in req.body == false ) {
+    return res.status(400).json({
+        status: 'error',
+        error: 'Missing gained_xp value from body'
+    });
+  }
+
+  skills.editGains(req.body.username, req.body.gained_xp);
+}),
+
+app.post('/skills', (req, res) => {
+  
+  if(req.body.username == false ) {
+    return res.status(400).json({
+        status: 'error',
+        error: 'Missing username value from body'
+    });
+  }
+  if('xp' in req.body == false ) {
+      return res.status(400).json({
+          status: 'error',
+          error: 'Missing xp value from body'
+      });
+  }
+  if('level' in req.body == false ) {
+        return res.status(400).json({
+            status: 'error',
+            error: 'Missing level value from body'
+      });
+  }
+
+  skills.createSkills(req.body.username, req.body.xp, req.body.level)
+
+  res.sendStatus(201);
+}),
+
+app.put('/skills', (req, res) => {
   if('username' in req.body == false ) {
     return res.status(400).json({
         status: 'error',
@@ -57,18 +102,11 @@ app.post('/skills', (req, res) => {
       });
   }
 
-app.put('/skills', (req, res) => {
-  skills.editSkills (req.body.username, req.body.xp, req.body.level)
+  skills.editSkills (req.body.username, req.body.xp, req.body.level);
   res.sendStatus(200);
-})
+}),
 
-
-  skills.createSkills(req.body.username, req.body.xp, req.body.level)
-
-  res.sendStatus(201)
-})
-
-app.use(express.static('public', options));
+app.use(express.static('public', options)),
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- services/skills.js
+++ services/skills.js
@@ -1,77 +1,128 @@
 const { v4: uuidv4 } = require('uuid');
 
-let skills = [
-    {
-        id : "1",
-        username : "testi ukko",
-        xp : {
-            overall : '40245234',
-            attack : '2130300',
-            defence : '2130300',
-            strength : '2130300',
-            hitpoints : '2130300',
-            ranged : '2130300',
-            prayer : '2130300',
-            magic : '2130300',
-            cooking : '2130300',
-            woodcutting : '2130300',
-            fletching : '2130300',
-            fishing : '2130300',
-            firemaking : '2130300',
-            crafting : '2130300',
-            smithing : '2130300',
-            mining : '2130300',
-            herblore : '2130300',
-            agility : '2130300',
-            thieving : '2130300',
-            slayer : '2130300',
-            farming : '2130300',
-            runecraft : '2130300',
-            hunter : '2130300',
-            construction : '2130300'
-        },
-        level : {
-          overall : '1840',
-          attack : '80',
-          defence : '80',
-          strength : '80',
-          hitpoints : '80',
-          ranged : '80',
-          prayer : '80',
-          magic : '80',
-          cooking : '80',
-          woodcutting : '80',
-          fletching : '80',
-          fishing : '80',
-          firemaking : '80',
-          crafting : '80',
-          smithing : '80',
-          mining : '80',
-          herblore : '80',
-          agility : '80',
-          thieving : '80',
-          slayer : '80',
-          farming : '80',
-          runecraft : '80',
-          hunter : '80',
-          construction : '80'
-        }
+let gains = [
+  {
+    id : "1",
+    username : "testi ukko",
+    gained_xp : {
+      overall : '351141',
+      attack : '15267',
+      defence : '15267',
+      strength : '15267',
+      hitpoints : '15267',
+      ranged : '15267',
+      prayer : '15267',
+      magic : '15267',
+      cooking : '15267',
+      woodcutting : '15267',
+      fletching : '15267',
+      fishing : '15267',
+      firemaking : '15267',
+      crafting : '15267',
+      smithing : '15267',
+      mining : '15267',
+      herblore : '15267',
+      agility : '15267',
+      thieving : '15267',
+      slayer : '15267',
+      farming : '15267',
+      runecraft : '15267',
+      hunter : '15267',
+      construction : '15267'
     }
+  }
+]
+
+let skills = [
+  {
+    id : "1",
+    username : "testi ukko",
+    xp : {
+      overall : '40245234',
+      attack : '2130300',
+      defence : '2130300',
+      strength : '2130300',
+      hitpoints : '2130300',
+      ranged : '2130300',
+      prayer : '2130300',
+      magic : '2130300',
+      cooking : '2130300',
+      woodcutting : '2130300',
+      fletching : '2130300',
+      fishing : '2130300',
+      firemaking : '2130300',
+      crafting : '2130300',
+      smithing : '2130300',
+      mining : '2130300',
+      herblore : '2130300',
+      agility : '2130300',
+      thieving : '2130300',
+      slayer : '2130300',
+      farming : '2130300',
+      runecraft : '2130300',
+      hunter : '2130300',
+      construction : '2130300'
+    },
+    level : {
+      overall : '1840',
+      attack : '80',
+      defence : '80',
+      strength : '80',
+      hitpoints : '80',
+      ranged : '80',
+      prayer : '80',
+      magic : '80',
+      cooking : '80',
+      woodcutting : '80',
+      fletching : '80',
+      fishing : '80',
+      firemaking : '80',
+      crafting : '80',
+      smithing : '80',
+      mining : '80',
+      herblore : '80',
+      agility : '80',
+      thieving : '80',
+      slayer : '80',
+      farming : '80',
+      runecraft : '80',
+      hunter : '80',
+      construction : '80'
+    }
+  }
 ]
 
 module.exports = {
-  createSkills: (username, xp) => {
-    skills.push({
-      id: uuidv4(),
+  createGains: (username) => {
+    gains.push({
+      id : uuidv4(),
       username,
-      xp
+      gained_xp : null
+    });
+  },
+  getGainedXpById: (id) => gains.find(u => u.id == id),
+  getGainedXpByName: (username) => gains.find(u => u.username == username),
+  editGains: (username, gained_xp) => {
+    const result = gains.find(t =>t.username == username)
+
+    if(result !== undefined)
+    {
+      result.gained_xp = gained_xp;
+    }
+  },
+  createSkills: (username, xp, level) => {
+    skills.push({
+      id: uuidv4,
+      username,
+      xp,
+      level
     });
   },
   getAllSkills: () => skills,
   getSkillsById: (id) => skills.find(u => u.id == id),
   getSkillsByName: (username) => skills.find(u => u.username == username),
   editSkills: (username, xp, level) => {
-    const result = users.find(t =>t.username == username)
+    const result = skills.find(t =>t.username == username)
 
     if(result !== undefined)
     {
